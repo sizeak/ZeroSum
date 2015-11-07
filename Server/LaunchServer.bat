ae210c0cc24b38439982cb9b25abc5923b1140b5
Simon Jackson <sizeak@hotmail.com>
2015-11-07 17:27:04 +0000

    Experimental server java config to improve performance.

--- Server/LaunchServer.bat
+++ Server/LaunchServer.bat
@@ -3,7 +3,7 @@
 :: When setting the memory below make sure to include the amount of ram letter. M = MB, G = GB. Don't use 1GB for example, it's 1G ::
 
 :: This is 64-bit memory ::
-set memsixtyfour=2G
+set memsixtyfour=3G
 
 :: This is 32-bit memory - maximum 1.2G ish::
 set memthirtytwo=1G
@@ -17,5 +17,5 @@ if $SYSTEM_os_arch==x86 (
   echo OS is 64
   set mem=%memsixtyfour%
 )
-java -Xmx%mem% -XX:MaxPermSize=256M -jar forge-1.7.10-10.13.4.1541-1.7.10-universal.jar nogui
+java -Xmx%mem% -Xms%mem% -XX:+UseG1GC -XX:MaxGCPauseMillis=150 -XX:PermSize=256m -jar forge-1.7.10-10.13.4.1541-1.7.10-universal.jar nogui
 PAUSE
--- Server/LaunchServer.sh
+++ Server/LaunchServer.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-java -Xmx2G -XX:MaxPermSize=256M -jar forge-1.7.10-10.13.4.1541-1.7.10-universal.jar nogui
+java -Xmx3G -Xms3G -XX:+UseG1GC -XX:MaxGCPauseMillis=150 -XX:PermSize=256m -jar forge-1.7.10-10.13.4.1541-1.7.10-universal.jar nogui
